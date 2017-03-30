925a3b1394baba991e617bc8339d4c6dd7cfd06f
Valentin Funk <funk.valentin@gmail.com>
2017-03-30 11:18:48 +0200

    fix(build): Use babel to transpile

--- bin/scriptfodder-publish.js
+++ bin/scriptfodder-publish.js
@@ -1,1 +1,14 @@
-require('../src')(process.argv, process)
+#!/usr/bin/env node
+
+/* istanbul ignore next */
+try {
+  require('../dist')(process.argv, process)
+} catch (err) {
+  if (err.code === 'MODULE_NOT_FOUND') {
+    require('babel-register')
+    require('../src')(process.argv, process)
+  } else {
+    console.error(err)
+    process.exit(1)
+  }
+}
